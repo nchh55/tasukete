1a5e4a04dfa065249a28c37be324005034943e51
ChangHyun <1newstart@naver.com>
2019-09-22 05:11:53 +0900

    no message

--- 0_yondai/src/main/java/global/sesoc/tasukete/dao/ComplimentMapper.java
+++
@@ -1,5 +0,0 @@
-package global.sesoc.tasukete.dao;
-
-public interface ComplimentMapper {
-
-}
--- 0_yondai/src/main/java/global/sesoc/tasukete/dao/ComplimentRepository.java
+++
@@ -1,8 +0,0 @@
-package global.sesoc.tasukete.dao;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class ComplimentRepository {
-
-}
